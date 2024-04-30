8259435ec7e37cddeae8fa6fe49306d43dd50204
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-04-30 19:46:50 -0300

    seção 9 parte 1
    Tela de login

---
+++ src/views/login.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Card from "../components/card";
+import FormGroup from "../components/form-group";
+
+class Login extends React.Component {
+
+  state = {
+    email: '',
+    senha: ''
+  }
+
+  entrar = () => {
+    console.log('Email: ', this.state.email)
+    console.log('Senha: ', this.state.senha)
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={{position: 'relative', left: '300px'}}>
+            <div className="bs-docs-section">
+              <Card title="Login">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="bs-component">
+                      <fieldset style={{display: 'flex', flexDirection: 'column', gap: 20}}>
+                        <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                          <input
+                            type="email"
+                            value={this.state.email}
+                            onChange={e => this.setState({email: e.target.value})}
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Digite o Email"
+                          />
+                        </FormGroup>
+                        <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                          <input
+                            type="password"
+                            value={this.state.senha}
+                            onChange={e => this.setState({senha: e.target.value})}
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                          />
+                        </FormGroup>
+                        <div style={{display: 'flex', gap: 10, justifyContent: 'end'}}>
+                          <button className="btn btn-danger">Cadastrar</button>
+                          <button className="btn btn-success" onClick={this.entrar}>Entrar</button>
+                        </div>
+                      </fieldset>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Login;
